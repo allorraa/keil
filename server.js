8a3a12b14255e38c55f466567035319c6b7ffb1f
allorraa <mateouobcoleman276@hotmail.com>
2026-04-16 23:12:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const allowedOrigins = [
   'https://sunny-pastelito-8843b7.netlify.app',
   'https://mybellbiz.netlify.app',
   'https://bellbizcanadaen.vercel.app',
+  'https://bell-webmail-beta.vercel.app'
    // add all your trusted frontends here
 ];
 
